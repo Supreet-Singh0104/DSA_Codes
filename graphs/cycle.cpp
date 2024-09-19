23907e42639d7472837873c602d07b2e6776f6e7
Supreet Singh Chawla <sschawla0104@gmail.com>
2024-09-19 07:23:03 +0530

    graph2

--- graphs/cycle.cpp
+++ graphs/cycle.cpp
@@ -27,7 +27,40 @@ class graph{
             cout<<endl;
         }
     }
+
+    bool isCycleHelper(int src,int par,vector<bool>& vis){
+        vis[src]=true;
+        list<int> neighbours=l[src];
+        for(int v:neighbours){
+            if(!vis[v]){
+                if(isCycleHelper(v,src,vis)){
+                    return true;
+                }
+            }else{
+                if(par!=v){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+
+    bool isCycle(){
+        vector<bool> vis(V,false);
+        return isCycleHelper(0,-1,vis);
+    }
 };
+
 int main(){
+    int v=5;
+    graph g(v);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(0,3);
+    g.addEdge(1,2);
+    g.addEdge(3,4);
+    bool ans=g.isCycle();
+    cout<<ans;
     return 0;
 }
